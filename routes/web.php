2bff2b60544ae22beba51517914bf9bfb1835774
Scott Mankowitz <scott.mankowitz@stationmd.com>
2020-10-22 17:07:29 -0400

    Automatic Commit - HOTFIX

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
